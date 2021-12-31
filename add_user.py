b5a1dc103c42d123ec736fd0c76e20bf5629170f
lucakreativ <luca.kjell.e@gmail.com>
2021-12-31 10:39:14 +0100

    add user

---
+++ add_user.py
@@ -0,0 +1,28 @@
+import sys
+from mysql.connector import MySQLConnection, Error
+from python_mysql_dbconfig import read_db_config
+from get_data import hash_password
+
+dbconfig = read_db_config()
+conn = MySQLConnection(**dbconfig)
+cursor = conn.cursor()
+
+type="Personal"
+
+if len(sys.argv)!=4:
+    print("zu much/zu less arguments")
+
+username=sys.argv[1]
+EMail=sys.argv[2]
+password=sys.argv[3]
+
+print("Username: "+username)
+print("EMail: "+EMail)
+print("Password: "+password)
+
+hash=hash_password(password)
+print("Hash: "+ hash)
+cursor.execute("""INSERT INTO Benutzer (Benutzername, EMail, Passwort, Typ) VALUES (%s, %s, %s, %s)""", (username, EMail, hash, type))
+conn.commit()
+
+print("success")
