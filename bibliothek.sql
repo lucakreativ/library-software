044c147b70a50e48253a2e4d02db794e729daa78
lucakreativ <luca.kjell.e@gmail.com>
2022-01-17 21:38:50 +0100

    standard database

---
+++ bibliothek.sql
@@ -0,0 +1,143 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for Linux (x86_64)
+--
+-- Host: localhost    Database: bibliothek-empty
+-- ------------------------------------------------------
+-- Server version	8.0.27-0ubuntu0.21.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Ausleihen`
+--
+
+DROP TABLE IF EXISTS `Ausleihen`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Ausleihen` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `Schülername` varchar(200) NOT NULL,
+  `ISBN` bigint NOT NULL,
+  `Datum` date NOT NULL,
+  `Verlängert` int NOT NULL,
+  `ProtokollID` int NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=278 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Benutzer`
+--
+
+DROP TABLE IF EXISTS `Benutzer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Benutzer` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `Benutzername` varchar(200) NOT NULL,
+  `EMail` varchar(200) NOT NULL,
+  `Passwort` varchar(200) NOT NULL,
+  `Typ` varchar(200) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Bücher`
+--
+
+DROP TABLE IF EXISTS `Bücher`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Bücher` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `ISBN` varchar(200) NOT NULL,
+  `Titel` varchar(255) NOT NULL,
+  `Autor` varchar(255) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=5133 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Computerraum`
+--
+
+DROP TABLE IF EXISTS `Computerraum`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Computerraum` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `Schülername` varchar(200) NOT NULL,
+  `Computernummer` int NOT NULL,
+  `Startzeit` time NOT NULL,
+  `ProtokollID` int NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Fakten`
+--
+
+DROP TABLE IF EXISTS `Fakten`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Fakten` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `Fakt` varchar(200) NOT NULL,
+  KEY `1` (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=1250 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `LoginIP`
+--
+
+DROP TABLE IF EXISTS `LoginIP`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `LoginIP` (
+  `Benutzername` varchar(500) NOT NULL,
+  `IP` varchar(500) NOT NULL,
+  `Datum` date NOT NULL,
+  `Anfangsuhrzeit` time NOT NULL,
+  `Enduhrzeit` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Protokoll`
+--
+
+DROP TABLE IF EXISTS `Protokoll`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Protokoll` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `ProtokollID` int NOT NULL,
+  `Art` int NOT NULL,
+  `Text` varchar(10000) NOT NULL,
+  `gebucht` varchar(200) NOT NULL,
+  `Datum` datetime NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2493 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-01-17 21:36:43
