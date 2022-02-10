074f7442e6d67cd1ff10e34b6ba5cd2d99fe93f9
lucakreativ <lucaeckenfels@gmx.de>
2022-02-10 20:12:42 +0100

    commended

--- protocol_write.py
+++ protocol_write.py
@@ -1,6 +1,7 @@
 from mysql.connector import MySQLConnection
-from read_config import read_db_config
 from datetime import date, datetime, time
+from read_config import read_db_config              # --> read_config.py
+
 
 
 def re_connect():                                   #setzt eine neue Verbindung, wegen MySQL timeout
@@ -11,34 +12,34 @@ def re_connect():                                   #setzt eine neue Verbindung,
     return cursor, conn                             #gibt den Cursor und die Verbindung zurück
 
 
-def write_in_ip_table(user, ip):
+def write_in_ip_table(user, ip):#schreibt Login-Daten + IP
     cursor, conn = re_connect()                     #bekommt MySQL Verbindungsdaten
 
-    today=date.today()
-    now = datetime.now()
-    current_time=time(hour=now.hour, minute=now.minute, second=now.second)
-    end=time(hour=0, minute=0, second=0)
+    today=date.today()#bekommt den heutigen Tag
+    now = datetime.now()#bekommt den heutigen Tag + Uhrzeit
+    current_time=time(hour=now.hour, minute=now.minute, second=now.second)#bekommt die Uhrzeit
+    end=time(hour=0, minute=0, second=0)#Endzeit, provisorisch
 
-    cursor.execute("""INSERT INTO LoginIP (Benutzername, IP, Datum, Anfangsuhrzeit, Enduhrzeit) VALUES (%s, %s, %s, %s, %s)""", (user, ip, today, current_time, end))
-    conn.commit()
+    cursor.execute("""INSERT INTO LoginIP (Benutzername, IP, Datum, Anfangsuhrzeit, Enduhrzeit) VALUES (%s, %s, %s, %s, %s)""", (user, ip, today, current_time, end))#übergibt Benutzername, IP, Datum, Uhrzeit und Enduhrheit
+    conn.commit()#speichert Änderungen
 
-def write_in_protocol_table(type, name, user, param):
+def write_in_protocol_table(type, name, user, param):#schreibt in das Protokoll
     cursor, conn = re_connect()#bekommt MySQL Verbindungsdaten
 
-    cursor.execute("""SELECT max(ProtokollID) FROM Protokoll""")
-    id=cursor.fetchall()[0][0]
-    if id==None:
+    cursor.execute("""SELECT max(ProtokollID) FROM Protokoll""")#bekommt Protokoll-ID
+    id=cursor.fetchall()[0][0]#speichert sie
+    if id==None:#Tabelle ist leer
         id=0
-    else:
+    else:#Tabelle ist nicht leer
         id+=1
 
-    type=int(type)
+    type=int(type)#Typ vom 
     if type==2:
-        now = datetime.now()
-        text="%s leiht %s von %s" % (name, param, now)
+        now = datetime.now()#Datum + Uhrzeit
+        text="%s leiht %s von %s" % (name, param, now)#vorgefertigter Sting
 
 
-    cursor.execute("""INSERT INTO Protokoll (ProtokollID, Art, Text, gebucht, Datum) VALUES (%s, %s, %s, %s, %s)""", (id, int(type), str(text), str(user), now))
-    conn.commit()
+    cursor.execute("""INSERT INTO Protokoll (ProtokollID, Art, Text, gebucht, Datum) VALUES (%s, %s, %s, %s, %s)""", (id, int(type), str(text), str(user), now))#übergibt Protokoll-ID, Typ, vorgefertigter String, Verleihname, Datum
+    conn.commit()#speichert Änderungen
 
     return id
